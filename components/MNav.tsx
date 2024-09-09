372ab8c99fcb38739995e9c5953280d924746565
SYED AMEER JUNAID <junaid0825@gmail.com>
2024-09-09 20:54:56 +0530

    forth

--- components/MNav.tsx
+++ components/MNav.tsx
@@ -9,14 +9,13 @@ import {
 import SearchInput from './SearchInput'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
+import { HomeIcon } from '@radix-ui/react-icons'
 function MNav() {
     return (
         <nav className='w-full flex  flex-row my-7 justify-between'>
             <div className='pl-5 my-auto'>
                 <Link href={'/'}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                    </svg>
+                    <HomeIcon className='h-7 w-7 my-auto' />
                 </Link>
             </div>
             <div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,6 +9,7 @@ import {
 import SearchInput from './SearchInput'
 import Link from 'next/link'
 import MNav from './MNav'
+import { HomeIcon } from '@radix-ui/react-icons'
 function Nav() {
     return (
         <>
@@ -17,11 +18,8 @@ function Nav() {
             </div>
             <div className='hidden md:block'>
                 <nav className='w-full flex my-7 justify-cente'>
-                    <Link href={'/'}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                        </svg>
-
+                    <Link href={'/'} className='my-auto'>
+                        <HomeIcon className='h-7 w-7 my-auto' />
                     </Link>
                     <NavigationMenu>
                         <NavigationMenuList>
