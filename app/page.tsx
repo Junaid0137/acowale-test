eb90da4ee1ec91ea30388550c6a58ba0089cca92
SYED AMEER JUNAID <junaid0825@gmail.com>
2024-09-09 23:45:27 +0530

    twelve

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Topics from "@/components/Topics";
 export default function Home() {
   return (
     <main>
+      <header>
+        <title>Junaid Acowale News</title>
+      </header>
       <Topics />
       <Highlights />
     </main>
